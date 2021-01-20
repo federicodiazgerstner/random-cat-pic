3f23ffc26a6a20a3dfcac31ed882025d63a1d675
Federico <federicodiazgerstner@gmail.com>
2021-01-19 22:07:17 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
 
 function App() {
+
+  const [data, setData] = useState({});
+  const [loadings , setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData()
+  }, [] );
+
+
+
+  async function fetchData() {
+    const getData = await fetch("https://api.thecatapi.com/v1/images/search")
+
+    const json = await getData.json()
+
+    setData(json[0].url);
+    setLoading(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>{loadings == true ? (
+    <p>Cargando</p>) : 
+    (<>
+      <img src={data} alt="cat image" />
+      <br />
+      <button onClick={fetchData}>Ver otra imagen</button>
+    </>)}
+    </>
+  )
 }
 
 export default App;
